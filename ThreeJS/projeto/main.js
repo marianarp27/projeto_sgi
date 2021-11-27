d24d0029d34a16d27ba24c5ae8011aa2521953f3
Belisa <belisalopes@hotmail.com>
2021-11-27 11:41:08 +0000

    JS updated

--- ThreeJS/projeto/main.js
+++ ThreeJS/projeto/main.js
@@ -1,5 +1,6 @@
 //cena
 var cena = new THREE.Scene();
+cena.background = new THREE.Color( 0xffffff );
 
 //canvas
 var meuCanvas = document.getElementById( 'meuCanvas' );
@@ -11,8 +12,10 @@ var relogio = new THREE.Clock();
 var misturador = new THREE.AnimationMixer(cena);
 
 //Ações
-var acaolocy;
-var acaoRotz;
+var acaoBenchExtend;
+var acaoLegExtend;
+var acaoLeftDoor;
+var acaoRightDoor;
 
 //VARIÁVEIS PARA BOTÕES HTML
 var ativar = document.getElementById('btn_play');
@@ -38,8 +41,8 @@ renderer.render( cena, camara );
 var controlos = new THREE.OrbitControls( camara, renderer.domElement );
 
 //gridHelper = grelha/chão de referência
-var grelha = new THREE.GridHelper();
-cena.add( grelha );
+//var grelha = new THREE.GridHelper();
+//cena.add( grelha );
 
 //função de animação
 function animar() { 
@@ -58,58 +61,83 @@ function ( gltf )
     if (x instanceof THREE.Light) 
     x.visible = false });
     cena.add( gltf.scene ) 
-    //clipe animação LOCY
-    clipeLocy = THREE.AnimationClip.findByName( gltf.animations, 'LocY' );
-    acaolocy = misturador.clipAction( clipeLocy ); 
-    //acaolocy.play();//fim clipe animação LOCY - removido no ponto 4
-    //clipe animação ROTZ
-    clipeRotz = THREE.AnimationClip.findByName( gltf.animations, 'RotZ' );
-    acaoRotz = misturador.clipAction( clipeRotz ); 
-    //acaoRotz.play();//fim clipe animação ROTZ - removido no ponto 4
-
+    //cena = new THREE.Mesh(stoneBenchM); - mexer para a textura
+    //clipe animação BenchExtend
+    clipeBenchExtend = THREE.AnimationClip.findByName( gltf.animations, 'BenchExtend' );
+    acaoBenchExtend = misturador.clipAction( clipeBenchExtend ); 
+    //clipe animação LegExtend
+    clipeLegExtend = THREE.AnimationClip.findByName( gltf.animations, 'LegExtend' );
+    acaoLegExtend = misturador.clipAction( clipeLegExtend ); 
+    //clipe animação LeftDoor
+    clipeLeftDoor = THREE.AnimationClip.findByName( gltf.animations, 'LeftDoor' );
+    acaoLeftDoor = misturador.clipAction( clipeLeftDoor ); 
+    //clipe animação RightDoor
+    clipeRightDoor = THREE.AnimationClip.findByName( gltf.animations, 'RightDoor' );
+    acaoRightDoor = misturador.clipAction( clipeRightDoor ); 
 } );
     
 
 //pontos luz
 var luzPonto1 = new THREE.PointLight( "white" );
-luzPonto1.position.set( 5, 3, 5 );
+luzPonto1.position.set( 5, 3, 5 ); //original 5,3,5
 cena.add( luzPonto1 );
+
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+cena.add( light );
 
 //ANIMAÇÕES DOS BOTÕES
 //PLAY
 function fazerplay(){
-    acaoRotz.play();
+    acaoBenchExtend.play();
+    acaoLegExtend.play();
+    acaoLeftDoor.play();
+    acaoRightDoor.play();
 }
 ativar.addEventListener("click", fazerplay);
 
 //STOP
 function fazerstop(){
-    acaoRotz.stop();
+    acaoBenchExtend.stop();
+    acaoLegExtend.stop();
+    acaoLeftDoor.stop();
+    acaoRightDoor.stop();
 }
 parar.addEventListener("click",fazerstop);
 
 //PAUSA
 function fazerpausa(){
-    acaoRotz.paused = true;
+    acaoBenchExtend.paused = true;
+    acaoLegExtend.paused = true;
+    acaoLeftDoor.paused = true;
+    acaoRightDoor.paused = true;
 }
 pausar.addEventListener("click", fazerpausa);
 
 //RESET COM TIMESCALE
 function reverteranimacao(){
-    acaoRotz.timeScale = -1;
+    acaoBenchExtend.timeScale = -1;
+    acaoLegExtend.timeScale = -1;
+    acaoLeftDoor.timeScale = -1;
+    acaoRightDoor.timeScale = -1;
 }
 reverter.addEventListener("click", reverteranimacao);
 
 // ciclo setLoop
 function loop(){
     if(ciclo.value == '1'){
-        acaoRotz.setLoop(THREE.LoopOnce);
+        acaoBenchExtend.setLoop(THREE.LoopOnce);
+        acaoLegExtend.setLoop(THREE.LoopOnce);
+        acaoLeftDoor.setLoop(THREE.LoopOnce);
+        acaoRightDoor.setLoop(THREE.LoopOnce);
     }
     if(ciclo.value == '2'){
-        acaoRotz.setLoop(THREE.LoopRepeat);
+        acaoLegExtend.setLoop(THREE.LoopRepeat);
     }
     if(ciclo.value == '3'){
-        acaoRotz.setLoop(THREE.LoopPingPong);
+        acaoBenchExtend.setLoop(THREE.LoopPingPong);
+        acaoLegExtend.setLoop(THREE.LoopPingPong);
+        acaoLeftDoor.setLoop(THREE.LoopPingPong);
+        acaoRightDoor.setLoop(THREE.LoopPingPong);
     }
 }
 ciclo.addEventListener("click",loop);
